64c3ee4ea53b3f26b203d74dd7cdc0a17f2eeec8
Tanya Melnikova <47141894+tanya124@users.noreply.github.com>
2023-05-24 23:45:01 +0300

    Fix

--- apply_manifests.sh
+++ apply_manifests.sh
@@ -8,8 +8,6 @@ minikube kubectl -- delete -f ingress_virtual_service.yaml
 minikube kubectl -- delete -f egress_service_entry.yaml
 minikube kubectl -- delete -f egress_gateway.yaml
 minikube kubectl -- delete -f egress_virtual_service.yaml
-#kubectl delete -f ingress_tls_gateway.yaml
-#kubectl delete -f ingress_tls_virtual_service.yaml
 
 
 minikube kubectl -- apply -f app.yaml
@@ -18,5 +16,3 @@ minikube kubectl -- apply -f ingress_virtual_service.yaml
 minikube kubectl -- apply -f egress_service_entry.yaml
 minikube kubectl -- apply -f egress_gateway.yaml
 minikube kubectl -- apply -f egress_virtual_service.yaml
-#kubectl apply -f ingress_tls_gateway.yaml
-#kubectl apply -f ingress_tls_virtual_service.yaml
